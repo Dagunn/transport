b76f71e7e5cb78cc33e26e7f4cfed09a304a2a17
Dagunn <dagunandroid@gmail.com>
2021-04-24 16:50:47 +0300

    created
    created

---
+++ app/js/main.js
@@ -0,0 +1,25 @@
+$(function () {
+
+    $('.filter').selectmenu();
+    // $(".filter").selectmenu({
+    //     classes: {
+    //         "ui-selectmenu-text": "my-happy-decorated-class"
+    //     }
+    //   });
+
+    $('.checkbox__label').on('click', function(){
+        $('.temperature__inner').fadeToggle('active')
+        
+    })
+     
+    $('#ui-id-1-button').on('click', function(){
+        $('.weight').toggleClass('active')
+    })
+
+    $('#ui-id-2-button').on('click', function(){
+        $('.shipment').toggleClass('active')
+    })
+   
+   
+    
+})
